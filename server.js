83f6e852fbfdc432ffabbf0c3b0bd84b61f7733f
Anand <anand@unilend.finance>
2021-02-14 12:07:16 +0530

    Heroku Deployment Setup

---
+++ server.js
@@ -0,0 +1,13 @@
+
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
+const path = require('path');
+app.use(express.static(__dirname +'/build'))
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port, function () {
+ console.log('App listening on port !');
+});
