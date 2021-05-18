b575a2867f675dcc264feb8d95ea55acefcc8d5f
Thanush Praveen <thanushpraveen@aequalisys.com>
2021-05-18 17:05:25 +0530

    Netlify setup

--- src/components/View/Borrow/Borrow.tsx
+++ src/components/View/Borrow/Borrow.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { FC, useEffect, useState } from "react";
 import ContentCard from "../UI/ContentCard/ContentCard";
 import FieldCard from "../UI/FieldsCard/FieldCard";
